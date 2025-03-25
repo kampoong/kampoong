ba967bc453bfb3f50f83412aa2325ab950d326ae
Agus Suhartono <agus.suhartono@gmail.com>
2025-03-26 05:29:34 +0700

    rename config/masjid.php to config/kampoong.php
    rename config/masjid.php to config/kampoong.php

---
+++ config/kampoong.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'name' => env('KAMPOONG_NAME', 'Kampoongku'),
+
+    'default_book_id' => env('KAMPOONG_DEFAULT_BOOK_ID', 1),
+
+    'income_color' => env('KAMPOONG_INCOME_COLOR', '#00AABB'),
+
+    'spending_color' => env('KAMPOONG_SPENDING_COLOR', '#F16867'),
+];
